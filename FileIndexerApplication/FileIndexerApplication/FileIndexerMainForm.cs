91e7280df05135815035b60073887cd160ee9793
IliaAnastassov <ilia.anastassov@gmail.com>
2016-07-22 13:18:32 +0300

    Add general layout

--- FileIndexerApplication/FileIndexerApplication/FileIndexerMainForm.cs
+++ FileIndexerApplication/FileIndexerApplication/FileIndexerMainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FileIndexerApplication
 {
-    public partial class Form1 : Form
+    public partial class FileIndexerMainForm : Form
     {
-        public Form1()
+        public FileIndexerMainForm()
         {
             InitializeComponent();
         }
