12ab2d97037f7792f225d6aea7a0fa3ad25600b6
IliaAnastassov <ilia.anastassov@gmail.com>
2016-08-02 17:18:22 +0300

    Refactor FileIndexer class

--- FileIndexerApplication/FileIndexerApplication/Models/FileIndexer.cs
+++ FileIndexerApplication/FileIndexerApplication/Models/FileIndexer.cs
@@ -10,29 +10,6 @@
 
     public class FileIndexer
     {
-        public static void SaveIndexTree(TreeView tree, string filename)
-        {
-            using (Stream file = File.Open(filename, FileMode.Create))
-            {
-                BinaryFormatter bf = new BinaryFormatter();
-                bf.Serialize(file, tree.Nodes.Cast<TreeNode>().ToList());
-            }
-        }
-
-        public static void LoadIndexTree(TreeView tree, string filename)
-        {
-            using (Stream file = File.Open(filename, FileMode.Open))
-            {
-                BinaryFormatter bf = new BinaryFormatter();
-                var obj = bf.Deserialize(file);
-
-                TreeNode[] nodes = (obj as IEnumerable<TreeNode>).ToArray();
-                tree.Nodes.Clear();
-                tree.Nodes.AddRange(nodes);
-                tree.Nodes[0].Expand();
-            }
-        }
-
         public static void SaveTree(FIDirectory tree, string filename)
         {
             using (Stream file = File.Open(filename, FileMode.Create))
