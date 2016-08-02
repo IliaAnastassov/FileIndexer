e0f28a14cc5456ebb7932b1103b906717fea605e
IliaAnastassov <ilia.anastassov@gmail.com>
2016-08-02 14:35:44 +0300

    Add ExtractItems method to the FileIndexer class

--- FileIndexerApplication/FileIndexerApplication/Models/FileIndexer.cs
+++ FileIndexerApplication/FileIndexerApplication/Models/FileIndexer.cs
@@ -32,5 +32,50 @@
                 tree.Nodes[0].Expand();
             }
         }
+
+        public static void SaveTree(DirectoryInfo root, string filename)
+        {
+            // Create a new fiDirectory object and populate it with
+            // info from the DirectoryInfo object
+            var fiRoot = new FIDirectory(root.FullName, root.Name, root.LastWriteTime);
+
+
+        }
+
+        private void ExtractItems(DirectoryInfo dir, FIDirectory fiDir)
+        {
+            // Extract needed information about the containing files of the DirectoryInfo object
+            // and store it in the FIDirectory object
+            foreach (var file in dir.GetFiles())
+            {
+                if (file.Attributes.HasFlag(FileAttributes.Hidden))
+                {
+                    continue;
+                }
+                fiDir.AddFile(new FIFile(file.Name, file.FullName, file.Extension, file.LastWriteTime, file.Length));
+            }
+
+            try
+            {
+                // Recursively extract needed information about all the subdirectories and their
+                // subdirectories of the Directoryinfo object and save it to the FIDirectory object
+                foreach (var subdir in dir.GetDirectories())
+                {
+                    if (subdir.Attributes.HasFlag(FileAttributes.Hidden))
+                    {
+                        continue;
+                    }
+
+                    var fiSubdir = new FIDirectory(subdir.FullName, subdir.Name, subdir.LastWriteTime);
+                    fiDir.AddSubdirectory(fiSubdir);
+
+                    ExtractItems(subdir, fiSubdir);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
