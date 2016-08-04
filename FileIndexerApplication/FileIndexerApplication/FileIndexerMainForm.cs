12b322d80caf29a5a9255143b0db0c6bb1a33cba
IliaAnastassov <ilia.anastassov@gmail.com>
2016-08-04 18:05:47 +0300

    Remove unnecessary methods from MainForm class

--- FileIndexerApplication/FileIndexerApplication/FileIndexerMainForm.cs
+++ FileIndexerApplication/FileIndexerApplication/FileIndexerMainForm.cs
@@ -5,9 +5,10 @@
     using System.Drawing;
     using System.IO;
     using System.Windows.Forms;
+    using Core;
     using Models;
+    using Factories;
 
-    // TODO: Add file icons to the loaded tree
     // TODO: Add search functionality in the indexed folder
     // TODO: Add command line arguments
     // TODO: Add copy/paste keyboard shortcuts to path text box
@@ -15,8 +16,15 @@
     {
         private string currentPath;
         private List<string> subsequentPaths = new List<string>(8);
-        private FIDirectory loadedDirectory;
+        private static FIDirectory loadedDirectory;
         private bool isLive = true; // Application state is set to LIVE by default
+
+        public static FIDirectory LoadedDirectory
+        {
+            get { return loadedDirectory; }
+
+            private set { loadedDirectory = value; }
+        }
 
         public FileIndexerMainForm()
         {
@@ -35,7 +43,7 @@
             // Populate the tree view with the default root folder
             MainFormTreeView.Nodes.Add(root);
 
-            GetTreeViewFolders(root);
+            FolderGenerator.GetFolders(root);
             root.Expand();
         }
 
@@ -45,7 +53,8 @@
 
             subsequentPaths.Add(currentPath);
             UpdatePathTextBox();
-            PopulateListViewStrategy(currentPath, isLive);
+
+            ViewGenerator.GenerateListView(currentPath, MainFormListView, isLive);
         }
 
         private void GoToButton_Click(object sender, EventArgs e)
@@ -57,8 +66,10 @@
             if (currentPath.Replace(" ", string.Empty) != string.Empty)
             {
                 subsequentPaths.Add(currentPath);
-                PopulateTreeViewStrategy(currentPath, isLive);
-                PopulateLiveListView(currentPath);
+
+                ViewGenerator.GenerateTreeView(currentPath, MainFormTreeView, isLive);
+                ViewGenerator.GenerateListView(currentPath, MainFormListView, isLive);
+
                 UpdatePathTextBox();
             }
         }
@@ -69,11 +80,11 @@
             {
                 subsequentPaths.RemoveAt(subsequentPaths.Count - 1);
                 currentPath = subsequentPaths[subsequentPaths.Count - 1];
-                PopulateLiveListView(currentPath);
+                ViewGenerator.GenerateListView(currentPath, MainFormListView, isLive);
 
                 if (DirectoryChanged())
                 {
-                    PopulateTreeViewStrategy(currentPath, isLive);
+                    ViewGenerator.GenerateTreeView(currentPath, MainFormTreeView, isLive);
                 }
 
                 UpdatePathTextBox();
@@ -131,15 +142,15 @@
             {
                 try
                 {
-                    loadedDirectory = FileIndexer.LoadTree(dialog.FileName);
+                    LoadedDirectory = FileIndexer.LoadTree(dialog.FileName);
 
-                    currentPath = loadedDirectory.Path;
+                    currentPath = LoadedDirectory.Path;
                     subsequentPaths.Add(currentPath);
                     UpdatePathTextBox();
                     isLive = false; // Set application state to LOAD
 
-                    PopulateTreeViewStrategy(currentPath, isLive);
-                    PopulateListViewStrategy(currentPath, isLive);
+                    ViewGenerator.GenerateTreeView(currentPath, MainFormTreeView, isLive);
+                    ViewGenerator.GenerateListView(currentPath, MainFormListView, isLive);
                 }
                 catch (Exception ex)
                 {
@@ -169,197 +180,7 @@
         {
             PathTextBox.Text = currentPath;
         }
-
-        private void PopulateTreeViewStrategy(string path, bool stateLive)
-        {
-            if (stateLive)
-            {
-                PopulateLiveTreeView(path);
-            }
-            else
-            {
-                PopulateLoadedTreeView(path);
-            }
-        }
-
-        private void PopulateLoadedTreeView(string path)
-        {
-            var dir = FileIndexer.FindFIDirectory(path, loadedDirectory);
-
-            if (dir != null)
-            {
-                TreeNode root = new TreeNode(dir.Name);
-
-                MainFormTreeView.Nodes.Clear();
-
-                root.Tag = dir.Path;
-                root.ImageIndex = dir.ImageIndex;
-                GetTreeViewFolders(root, dir);
-
-                MainFormTreeView.Nodes.Add(root);
-                root.Expand();
-            }
-        }
-
-        private void PopulateLiveTreeView(string path)
-        {
-            TreeNode root;
-            var dir = new DirectoryInfo(path);
-
-            if (dir.Exists)
-            {
-                // Clear the existing nodes only in case of a valid path
-                MainFormTreeView.Nodes.Clear();
-
-                root = new TreeNode(dir.Name);
-                root.Tag = dir;
-                GetTreeViewFolders(root);
-
-                MainFormTreeView.Nodes.Add(root);
-                root.Expand();
-            }
-        }
-
-        private void PopulateListViewStrategy(string path, bool stateLive)
-        {
-            if (stateLive)
-            {
-                PopulateLiveListView(path);
-            }
-            else
-            {
-                PopulateLoadedListView(path);
-            }
-        }
-
-        private void PopulateLiveListView(string path)
-        {
-            var dir = new DirectoryInfo(path);
-
-            MainFormListView.Items.Clear();
-            LargeImageList.Images.RemoveByKey(".exe");
-            SmallIimageList.Images.RemoveByKey(".exe");
-
-            // Get the files in the selected directory and display them in the list view
-            foreach (var file in dir.GetFiles())
-            {
-                // Don't add hidden files
-                if (file.Attributes.HasFlag(FileAttributes.Hidden))
-                {
-                    continue;
-                }
-
-                var item = new ListViewItem(file.Name);
-
-                // Populate the sub-items of the item
-                var lastModified = file.LastWriteTime;
-                item.SubItems.Add(lastModified.ToShortDateString() + " " + lastModified.ToShortTimeString());
-                item.SubItems.Add(file.Extension);
-                item.SubItems.Add(Math.Ceiling(file.Length / 1024d) + " KB");
-
-                // Add an icon only once
-                if (!LargeImageList.Images.ContainsKey(file.Extension))
-                {
-                    var icon = Icon.ExtractAssociatedIcon(file.FullName);
-                    LargeImageList.Images.Add(file.Extension, icon);
-                    SmallIimageList.Images.Add(file.Extension, icon);
-                }
-
-                item.ImageKey = file.Extension;
-                MainFormListView.Items.Add(item);
-            }
-
-            // Get the folders in the selected directory and display them in the list view
-            foreach (var childDir in dir.GetDirectories())
-            {
-                // Don't add hidden directories
-                if (childDir.Attributes.HasFlag(FileAttributes.Hidden))
-                {
-                    continue;
-                }
-
-                // Create a list view item with the folder image index (0)
-                var item = new ListViewItem(childDir.Name, 0);
-
-                MainFormListView.Items.Add(item);
-            }
-        }
-
-        private void PopulateLoadedListView(string path)
-        {
-            MainFormListView.Items.Clear();
-
-            var dir = FileIndexer.FindFIDirectory(path, loadedDirectory);
-
-            // Extract files
-            foreach (var file in dir.Files)
-            {
-                var item = new ListViewItem(file.Name, file.ImageIndex);
-                item.SubItems.Add(file.LastModified.ToShortDateString() + " " + file.LastModified.ToShortTimeString());
-                item.SubItems.Add(file.Extension);
-                item.SubItems.Add(Math.Ceiling(file.Size / 1024d) + " KB");
-
-                MainFormListView.Items.Add(item);
-            }
-
-            // Extract directories
-            foreach (var subdir in dir.Subdirs)
-            {
-                var item = new ListViewItem(subdir.Name, subdir.ImageIndex);
-
-                MainFormListView.Items.Add(item);
-            }
-        }
-
-        private void GetTreeViewFolders(TreeNode node)
-        {
-            var dir = new DirectoryInfo(node.Tag.ToString());
-
-            try
-            {
-                // Recursively add all child nodes to the application tree view
-                foreach (var childDir in dir.GetDirectories())
-                {
-                    // If access to a folder is restricted don't display  it
-                    if (childDir.Attributes.HasFlag(FileAttributes.Hidden))
-                    {
-                        continue;
-                    }
-
-                    var childNode = new TreeNode(childDir.Name);
-                    childNode.Tag = childDir.FullName;
-                    node.Nodes.Add(childNode);
-
-                    GetTreeViewFolders(childNode);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void GetTreeViewFolders(TreeNode node, FIDirectory dir)
-        {
-            try
-            {
-                foreach (var subdir in dir.Subdirs)
-                {
-                    var childNode = new TreeNode(subdir.Name);
-                    childNode.Tag = subdir.Path;
-                    childNode.ImageIndex = subdir.ImageIndex;
-
-                    node.Nodes.Add(childNode);
-
-                    GetTreeViewFolders(childNode, subdir);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
+        
         private bool DirectoryChanged()
         {
             if (currentPath[0] != PathTextBox.Text[0])
