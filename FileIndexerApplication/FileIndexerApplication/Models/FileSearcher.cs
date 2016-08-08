268edaaf11c38bf0f4e9735183575f9ca4e3122a
IliaAnastassov <ilia.anastassov@gmail.com>
2016-08-08 14:36:49 +0300

    Add FileSearcher class

---
+++ FileIndexerApplication/FileIndexerApplication/Models/FileSearcher.cs
@@ -0,0 +1,24 @@
+﻿namespace FileIndexerApplication.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class FileSearcher
+    {
+        public static void SearchByname(FIDirectory dir, string fileName, List<FIFile> foundFiles)
+        {
+            foreach (var file in dir.Files)
+            {
+                if (file.Name == fileName)
+                {
+                    foundFiles.Add(file);
+                }
+            }
+
+            foreach (var subdir in dir.Subdirs)
+            {
+                SearchByname(subdir, fileName, foundFiles);
+            }
+        }
+    }
+}
