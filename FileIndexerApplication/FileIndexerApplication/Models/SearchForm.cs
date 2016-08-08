08b739f6f4d30799372194375a7e4c6d7b1ed199
IliaAnastassov <ilia.anastassov@gmail.com>
2016-08-08 14:38:10 +0300

    Add Search button functionality

--- FileIndexerApplication/FileIndexerApplication/Models/SearchForm.cs
+++ FileIndexerApplication/FileIndexerApplication/Models/SearchForm.cs
@@ -1,20 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace FileIndexerApplication.Models
+﻿namespace FileIndexerApplication.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Windows.Forms;
+
     public partial class SearchForm : Form
     {
+        private List<FIFile> foundFiles = new List<FIFile>();
+
+        public List<FIFile> FoundFiles
+        {
+            get { return this.foundFiles; }
+        }
+
         public SearchForm()
         {
             InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            if (FileNameTextBox.Text != null)
+            {
+                var fileName = FileNameTextBox.Text;
+                try
+                {
+                    FileSearcher.SearchByname(FileIndexerMainForm.LoadedDirectory, fileName, foundFiles);
+                }
+                catch (ArgumentException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            if (MaxSizeTextBox.Text != null)
+            {
+                // TODO
+            }
+            if (MinSizeTextBox.Text != null)
+            {
+                // TODO
+            }
+            if (DateModifiedTextBox.Text != null)
+            {
+                // TODO
+            }
+            if (FileExtensionTextBox.Text != null)
+            {
+                // TODO
+            }
+
+            this.Close();
         }
     }
 }
