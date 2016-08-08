3921a1e9edfc3c45ef394b66a9494846c1aeec53
IliaAnastassov <ilia.anastassov@gmail.com>
2016-08-08 15:42:54 +0300

    Add search method for each case

--- FileIndexerApplication/FileIndexerApplication/Models/FileSearcher.cs
+++ FileIndexerApplication/FileIndexerApplication/Models/FileSearcher.cs
@@ -2,10 +2,11 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class FileSearcher
     {
-        public static void SearchByname(FIDirectory dir, string fileName, List<FIFile> foundFiles)
+        public static void SearchByName(FIDirectory dir, string fileName, List<FIFile> foundFiles)
         {
             foreach (var file in dir.Files)
             {
@@ -17,7 +18,73 @@
 
             foreach (var subdir in dir.Subdirs)
             {
-                SearchByname(subdir, fileName, foundFiles);
+                SearchByName(subdir, fileName, foundFiles);
+            }
+        }
+
+        public static void SearchByMaxSize(FIDirectory dir, int fileSize, List<FIFile> foundFiles)
+        {
+            foreach (var file in dir.Files)
+            {
+                if (file.Size <= fileSize)
+                {
+                    foundFiles.Add(file);
+                }
+            }
+
+            foreach (var subdir in dir.Subdirs)
+            {
+                SearchByMaxSize(subdir, fileSize, foundFiles);
+            }
+        }
+
+        public static void SearchByMinSize(FIDirectory dir, int fileSize, List<FIFile> foundFiles)
+        {
+            foreach (var file in dir.Files)
+            {
+                if (file.Size >= fileSize)
+                {
+                    foundFiles.Add(file);
+                }
+            }
+
+            foreach (var subdir in dir.Subdirs)
+            {
+                SearchByMinSize(subdir, fileSize, foundFiles);
+            }
+        }
+
+        public static void SearchByDateModified(FIDirectory dir, DateTime date, List<FIFile> foundFiles)
+        {
+            foreach (var file in dir.Files)
+            {
+                if (file.LastModified == date)
+                {
+                    foundFiles.Add(file);
+                }
+            }
+
+            foreach (var subdir in dir.Subdirs)
+            {
+                SearchByDateModified(subdir, date, foundFiles);
+            }
+        }
+
+        public static void SearchByFileExtension(FIDirectory dir, string fileExtension, List<FIFile> foundFiles)
+        {
+            foreach (var file in dir.Files)
+            {
+                var extension = file.Name.Split('.').ToArray()[1];
+
+                if (extension == fileExtension)
+                {
+                    foundFiles.Add(file);
+                }
+            }
+
+            foreach (var subdir in dir.Subdirs)
+            {
+                SearchByFileExtension(subdir, fileExtension, foundFiles);
             }
         }
     }
