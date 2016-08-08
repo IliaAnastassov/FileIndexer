42a0633593471fbef25e138004dcd5ed5c123865
IliaAnastassov <ilia.anastassov@gmail.com>
2016-08-08 14:38:42 +0300

    Update project files

--- FileIndexerApplication/FileIndexerApplication/Models/SearchForm.Designer.cs
+++ FileIndexerApplication/FileIndexerApplication/Models/SearchForm.Designer.cs
@@ -148,6 +148,7 @@
             this.SearchButton.TabIndex = 1;
             this.SearchButton.Text = "Search";
             this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
             // 
             // SearchForm
             // 
