addeef6b745420847cb295cf5da02389c7ebf377
IliaAnastassov <ilia.anastassov@gmail.com>
2016-08-09 11:14:50 +0300

    Add file generator class

---
+++ FileIndexerApplication/FileIndexerApplication/Factories/FileGenerator.cs
@@ -0,0 +1,32 @@
+﻿namespace FileIndexerApplication.Factories
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using Models;
+
+    public class FileGenerator
+    {
+        public static List<FIFile> GetFiles(FIDirectory dir)
+        {
+            var files = new List<FIFile>();
+
+            ExtractFiles(dir, files);
+
+            return files;
+        }
+
+        private static void ExtractFiles(FIDirectory dir, List<FIFile> files)
+        {
+            foreach (var file in dir.Files)
+            {
+                files.Add(file);
+            }
+
+            foreach (var subdir in dir.Subdirs)
+            {
+                ExtractFiles(subdir, files);
+            }
+        }
+    }
+}
