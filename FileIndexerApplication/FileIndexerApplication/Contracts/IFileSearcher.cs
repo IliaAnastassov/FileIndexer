35b54a4e87396ece72e3322b30cddf89cb3f8c06
IliaAnastassov <ilia.anastassov@gmail.com>
2016-08-09 14:22:32 +0300

    Add IFileSearcher interface and FileSearcher class

---
+++ FileIndexerApplication/FileIndexerApplication/Contracts/IFileSearcher.cs
@@ -0,0 +1,19 @@
+﻿namespace FileIndexerApplication.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+    using Models;
+
+    public interface IFileSearcher
+    {
+        IList<FIFile> SearchByName(IList<FIFile> files, string name);
+
+        IList<FIFile> SearchByMaxSize(IList<FIFile> files, long size);
+
+        IList<FIFile> SearchByMinSize(IList<FIFile> files, long size);
+
+        IList<FIFile> SearchByDate(IList<FIFile> files, DateTime date);
+
+        IList<FIFile> SearchByExtension(IList<FIFile> files, string extension);
+    }
+}
---
+++ FileIndexerApplication/FileIndexerApplication/Models/FileSearcher.cs
@@ -0,0 +1,25 @@
+﻿namespace FileIndexerApplication.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Contracts;
+
+    public class FileSearcher : IFileSearcher
+    {
+        public IList<FIFile> SearchByName(IList<FIFile> files, string name)
+            => files.Where(f => f.Name.ToLower().Contains(name)).ToList();
+
+        public IList<FIFile> SearchByMaxSize(IList<FIFile> files, long size)
+            => files.Where(f => f.Size <= size).ToList();
+
+        public IList<FIFile> SearchByMinSize(IList<FIFile> files, long size)
+            => files.Where(f => f.Size >= size).ToList();
+
+        public IList<FIFile> SearchByExtension(IList<FIFile> files, string extension)
+            => files.Where(f => f.Extension == extension).ToList();
+
+        public IList<FIFile> SearchByDate(IList<FIFile> files, DateTime date)
+            => files.Where(f => f.LastModified == date).ToList();
+    }
+}
